f0d5263a2fa1580d6f0b34abb22c3b43af1c2097
M. Firhan Azmi Nor <muhammadfirhan@student.ub.ac.id>
2020-02-04 14:36:28 +0700

    summary

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <h1 style="color: #0000FF; font-size: 50px">Calculator</h1>
+    <Calculator/>
+  </div>
+</template>
+
+<script>
+import Calculator from './components/Calculator.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Calculator
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #ff6347;
+  margin-top: 60px;
+}
+</style>
